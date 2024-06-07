f0b259c6ea1e6bc28077fd3d1a14c434a85a200a
Kamlesh Kumar <67237252+kamleshsande85@users.noreply.github.com>
2024-06-07 20:11:41 +0530

    1

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('anniversarySong').addEventListener('ended', function() {
+    document.getElementById('container').style.display = 'none';
+    document.getElementById('heart').style.display = 'block';
+    document.getElementById('love').style.display = 'block';
+});
